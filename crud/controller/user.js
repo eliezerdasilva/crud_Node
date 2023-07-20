a39b576a37cdd4b0d970d96ab24b997b7101a2f4
Eliezer <frete7132@gmail.com>
2023-07-20 15:35:20 -0300

    inicio do projeto de um crud Utilizando Node.js

---
+++ crud/controller/user.js
@@ -0,0 +1,8 @@
+import {db} from "../db.js";
+export const getUsers = (_,res)=>{
+    const q = "Select * from usuarios";
+    db.query(q,(err,data)=>{
+        if(err)return res.json(err);
+        return res.status(200).json(data);
+    });
+}
